e73ddd8adacbb47aeacbafa91bdb49ede4634bc3
Andrew Binnie <binnieandrew@gmail.com>
2019-12-17 22:25:29 +0000

    adding in the currency list and input box

--- src/app.js
+++ src/app.js
@@ -4,19 +4,21 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
+      rates: {}
     },
     computed: {
 
     },
     mounted() {
 
+      this.getCurrencyRates()
     },
     methods: {
       getCurrencyRates: function(){
         fetch("https://api.exchangeratesapi.io/latest")
         .then(res => res.json())
-        .then(currencies => this.countries = currencies)
-      },
+        .then(data => this.rates = data.rates)
+      }
 
     }
 
